c4a8f9369a9408d741f5663bb60abd3195c4d2aa
Kaius Häggblom <leikaius.haggblom@gmail.com>
2026-08-10 11:59:56 +0300

    Saving

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use float_cmp::approx_eq;
-use std::collections::VecDeque;
+use std::vec::Vec;
 use std::ops::{Add, Sub};
+use std::io::stdout;
 use termion::{clear, color, cursor, style, terminal_size};
+use termion::raw::{IntoRawMode};
 
 pub enum Mode {
     Dec,
@@ -10,46 +12,46 @@ pub enum Mode {
     Bin,
 }
 
+#[derive(Debug)]
 pub struct Stack<T> {
-    pub stack_size: u16,
-    pub stack: VecDeque<T>,
+    pub stack: Vec<T>,
 }
 
 impl<T> Stack<T> {
-    pub fn new(stack_size: u16) -> Stack<T> {
-        let mut stack: VecDeque<T> = VecDeque::new();
+    pub fn new() -> Stack<T> {
+        let mut stack: Vec<T> = Vec::new();
         Stack {
-            stack_size: stack_size,
             stack: stack,
         }
     }
 
-    /// Pushes new item to stack
     pub fn push(&mut self, item: T) {
-        self.stack.push_front(item);
+        self.stack.push(item);
     }
 }
 
 impl<T: Add<Output = T>> Stack<T> {
     fn add(&mut self) {
-        let x = self.stack.remove(0).unwrap();
-        let y = self.stack.remove(0).unwrap();
-        self.stack.push_front(x + y);
+        let x = self.stack.pop().unwrap();
+        let y = self.stack.pop().unwrap();
+        self.stack.push(x + y);
     }
 }
 
 impl<T: Sub<Output = T>> Stack<T> {
     fn sub(&mut self) {
-        let x = self.stack.remove(0).unwrap();
-        let y = self.stack.remove(0).unwrap();
-        self.stack.push_front(y - x);
+        let x = self.stack.pop().unwrap();
+        let y = self.stack.pop().unwrap();
+        self.stack.push(y - x);
     }
 }
 
 pub fn render<T>(stack: Stack<T>) {
+    let mut stdout = stdout().into_raw_mode().unwrap();
     let (columns, rows) = terminal_size().unwrap();
-    for i in 1..stack.stack_size + 1 {
-        print!("{}{:03}:", cursor::Goto(1, stack.stack_size + 1 - i), i);
+    let stack_size:u16 = stack.stack.len() as u16;
+    for i in 1..stack_size+1 {
+        print!("{}{:03}:", cursor::Goto(1, stack_size + 1 - i), i);
     }
 }
 
@@ -59,7 +61,7 @@ mod tests {
 
     #[test]
     fn test_add_int() {
-        let mut stack: Stack<i64> = Stack::new(4);
+        let mut stack: Stack<i64> = Stack::new();
         stack.push(5);
         stack.push(6);
         stack.add();
@@ -70,7 +72,7 @@ mod tests {
 
     #[test]
     fn test_add_float() {
-        let mut stack: Stack<f64> = Stack::new(4);
+        let mut stack: Stack<f64> = Stack::new();
         stack.push(1.0);
         stack.push(3.14);
         stack.add();
@@ -79,7 +81,7 @@ mod tests {
 
     #[test]
     fn test_sub_int() {
-        let mut stack: Stack<i64> = Stack::new(4);
+        let mut stack: Stack<i64> = Stack::new();
         stack.push(10);
         stack.push(5);
         stack.sub();
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
-use rpntui;
+use rpntui::{Stack, render};
+use termion::event::Key;
+use termion::raw::IntoRawMode;
+use termion::input::TermRead;
+use termion::clear;
+use termion::terminal_size;
+use termion::cursor;
+use std::io::{Write, stdin, stdout};
 
 fn main() {
-    let mut stack = Stack<i64>::new();
-    println!("{:#?}", stack);
+    let mut stack: Stack<f64> = Stack::new();
+    stack.push(0.0);
+    stack.push(0.0);
+    stack.push(0.0);
+    stack.push(0.0);
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    write!(stdout, "{}", clear::All);
+    let (columns, rows) = terminal_size().unwrap();
+    write!(stdout, "{}{}x{}", cursor::Goto(0, rows), rows, columns);
+    render(stack);
+    stdout.flush();
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => break,
+            _  => print!("hups"),
+        }
+        stdout.flush().unwrap();
+    }
 }
